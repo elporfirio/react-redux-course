4a63092c9ebcad8b2351b2ed70c7fe080f24650e
Porfirio Chávez <elporfirio@gmail.com>
2018-05-04 13:15:51 -0500

    Preview Action names

--- src/actions/CourseActions.js
+++ src/actions/CourseActions.js
@@ -1,9 +1,22 @@
 import * as types from './ActionTypes';
+import courseApi from '../api/mockCourseApi';
 
 export function createCourse(course) {
   return {type: types.CREATE_COURSE, course: course};
 }
 
+export function loadCoursesSuccess(courses) {
+  return {type: types.LOAD_COURSES_SUCCESS, courses: courses};
+}
+
 export function loadCourses() {
   //
+  return (dispatch) => {
+    return courseApi.getAllCourses().then(courses => {
+      dispatch(loadCoursesSuccess(courses))
+        .catch(error => {
+          throw(error);
+        });
+    });
+  };
 }
